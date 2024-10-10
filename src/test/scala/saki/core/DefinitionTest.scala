32cee16d7cd8dcbc126ebbae355281956922b82c
RIvance <rivance.0@gmail.com>
2024-10-10 23:03:23 +0800

    feat: global evaluation statement

--- src/test/scala/saki/core/DefinitionTest.scala
+++ src/test/scala/saki/core/DefinitionTest.scala
@@ -4,18 +4,63 @@ import org.scalatest.flatspec.AnyFlatSpec
 import org.scalatest.matchers.*
 import saki.cli.compileModule
 
-class DefinitionTest extends AnyFlatSpec with should.Matchers {
+class DefinitionTest extends AnyFlatSpec with should.Matchers with SakiTestExt {
   it should "" in {
     val code = {
       """
-      |  type Bool = inductive {
-      |      True
-      |      False
-      |  }
-      |""".stripMargin
+        type Nat = inductive {
+            Zero
+            Succ(Nat)
+        }
+
+        def plus(a b : Nat): Nat = match a {
+            case Nat::Zero => b
+            case Nat::Succ(a') => Nat::Succ(plus(a', b))
+        }
+
+        def fib(n : Nat): Nat = match n {
+            case Nat::Zero => Nat::Zero
+            case Nat::Succ(Nat::Zero) => Nat::Succ(Nat::Zero)
+            case Nat::Succ(Nat::Succ(n')) => plus(fib(n'), fib(Nat::Succ(n')))
+        }
+
+        def n0: Nat = Nat::Zero
+        def n1: Nat = Nat::Succ(n0)
+        def n2: Nat = Nat::Succ(n1)
+        def n3: Nat = Nat::Succ(n2)
+        def n4: Nat = Nat::Succ(n3)
+        def n5: Nat = Nat::Succ(n4)
+        def n6: Nat = Nat::Succ(n5)
+        def n7: Nat = Nat::Succ(n6)
+        def n8: Nat = Nat::Succ(n7)
+        def n9: Nat = Nat::Succ(n8)
+      """
     }
     val module = compileModule(code)
-    module.definitions.foreach(println)
+
+    // Test add: 3 + 4 = 7
+    module.eval(parseExpr("plus(n3, n4)")) should be (module.eval(parseExpr("n7")))
+
+    // Test add: 0 + n = n
+    module.eval(parseExpr("plus(n0, n5)")) should be (module.eval(parseExpr("n5")))
+    module.eval(parseExpr("plus(n0, n0)")) should be (module.eval(parseExpr("n0")))
+
+    // Test add: n + 0 = n
+    module.eval(parseExpr("plus(n6, n0)")) should be (module.eval(parseExpr("n6")))
+
+    // Commutativity of addition: 2 + 3 = 3 + 2
+    module.eval(parseExpr("plus(n2, n3)")) should be (module.eval(parseExpr("plus(n3, n2)")))
+
+    // Test specific edge case: fibonacci of 0
+    module.eval(parseExpr("fib(n0)")) should be (module.eval(parseExpr("n0")))
+
+    // Test fibonacci: fib(1) = 1, fib(2) = 1, fib(3) = 2, fib(4) = 3, fib(5) = 5, fib(6) = 8
+    module.eval(parseExpr("fib(n1)")) should be(module.eval(parseExpr("n1")))
+    module.eval(parseExpr("fib(n2)")) should be(module.eval(parseExpr("n1")))
+    module.eval(parseExpr("fib(n3)")) should be(module.eval(parseExpr("n2")))
+    module.eval(parseExpr("fib(n4)")) should be(module.eval(parseExpr("n3")))
+    module.eval(parseExpr("fib(n5)")) should be(module.eval(parseExpr("n5")))
+    module.eval(parseExpr("fib(n6)")) should be(module.eval(parseExpr("n8")))
   }
 
 }
--- src/test/scala/saki/core/ExprTest.scala
+++ src/test/scala/saki/core/ExprTest.scala
@@ -1,66 +1,13 @@
 package saki.core
 
-import org.antlr.v4.runtime.{CharStreams, CommonTokenStream}
 import org.scalatest.flatspec.AnyFlatSpec
 import org.scalatest.matchers.should
-import saki.cli.catchError
-import saki.concrete.Visitor
-import saki.core.context.Environment
-import saki.core.domain.{NeutralValue, Value}
-import saki.core.syntax.Var
-import saki.grammar.{SakiLexer, SakiParser}
+import saki.core.domain.Value
 
-class ExprTest extends AnyFlatSpec with should.Matchers {
-
-  extension (str: String) def unary_! : Var.Local = Var.Local(str)
-
-  extension (literal: Literal) {
-    def term: Term.Primitive = Term.Primitive(literal)
-    def value: Value.Primitive = Value.Primitive(literal)
-  }
-
-  extension (literalType: LiteralType) {
-    def term: Term.PrimitiveType = Term.PrimitiveType(literalType)
-    def value: Value.PrimitiveType = Value.PrimitiveType(literalType)
-  }
-
-  extension (value: Value) {
-    def neutral: NeutralValue = value match {
-      case Value.Neutral(neutral) => neutral
-      case _ => throw new Exception("Not a neutral value.")
-    }
-
-    def apply(arg: Value): Value = value match {
-      case Value.Lambda(_, closure) => closure(arg)
-      case Value.Neutral(neutral) => Value.Neutral(NeutralValue.Apply(neutral, arg))
-      case _ => throw new Exception("Not a function.")
-    }
-  }
+class ExprTest extends AnyFlatSpec with should.Matchers with SakiTestExt {
 
   import Literal.*
   import LiteralType.*
-
-  given Environment.Typed[Value] = Environment.Typed.empty[Value]
-
-  def synthExpr(code: String): (Term, Value) = {
-    val stripedCode = code.strip()
-    val lexer = SakiLexer(CharStreams.fromString(stripedCode))
-    val parser = SakiParser(CommonTokenStream(lexer))
-    val exprTree = Visitor().visitExpr(parser.expr())
-    catchError(stripedCode) {
-      exprTree.emit.synth.unpack
-    }
-  }
-
-  def synthCodeBlock(code: String): (Term, Value) = {
-    val stripedCode = "{\n" + code.strip() + "\n}"
-    val lexer = SakiLexer(CharStreams.fromString(stripedCode))
-    val parser = SakiParser(CommonTokenStream(lexer))
-    val block = Visitor().visitBlockExpr(parser.blockExpr())
-    catchError(stripedCode) {
-      block.emit.synth.unpack
-    }
-  }
 
   it should "synth primitive type Int" in {
     val (term, ty) = synthExpr("1")
