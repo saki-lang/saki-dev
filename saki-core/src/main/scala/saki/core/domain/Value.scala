7e9a0c4415d0d4f8634ef8ee4c5e4d2a7174af59
RIvance <rivance.0@gmail.com>
2024-10-28 03:47:20 +0800

    fix(core): shouldn't read-back param type outside the environment with the newly generated variable

--- saki-core/src/main/scala/saki/core/domain/Value.scala
+++ saki-core/src/main/scala/saki/core/domain/Value.scala
@@ -376,10 +376,10 @@ object Value extends RuntimeEntityFactory[Value] {
   ): (Param[Term], Term) = {
     val paramIdent = env.uniqueVariable
     val paramVariable = Value.variable(paramIdent, paramType)
-    val term = env.withLocal(paramIdent, paramVariable, paramType) {
-      implicit env => closure(paramVariable).readBack(env)
+    env.withLocal(paramIdent, paramVariable, paramType) { implicit env =>
+      val term = closure(paramVariable).readBack(env)
+      (Param(paramIdent, paramType.readBack), term)
     }
-    (Param(paramIdent, paramType.readBack), term)
   }
 
   extension (self: InductiveType) {
@@ -400,10 +400,10 @@ private sealed trait OverloadedLambdaLike[S <: OverloadedLambdaLike[S] & Value] 
     states.map { (paramType, closure) =>
       val paramIdent = env.uniqueVariable
       val param = Value.variable(paramIdent, paramType)
-      val body = env.withLocal(paramIdent, param, paramType) {
-        closure(param).readBack
+      env.withLocal(paramIdent, param, paramType) { implicit env =>
+        val body = closure(param).readBack
+        (Param(paramIdent, paramType.readBack), body)
       }
-      (Param(paramIdent, paramType.readBack), body)
     }
   }
 
